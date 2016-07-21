0f9f7ab4db7c55b9dd80634bc6cdfa1f33922dd4
Erin Bergman <erin.e.bergman@gmail.com>
2016-07-21 14:19:19 -0400

    07/21/16 2:19p

---
+++ src/com/bergman/Notes.java
@@ -0,0 +1,11 @@
+//package com.bergman;
+//
+//public class Notes {
+//
+//	for(int i = 0; i < 5; i++) {
+//		
+//		
+//		
+//	}
+//	
+//}
--- src/com/bergman/Whatever.java
+++ src/com/bergman/Whatever.java
@@ -3,15 +3,9 @@ package com.bergman;
 public class Whatever {
 
 	public static void main(String[] args) {
-// &&
 		
-	/*
-	 * *
-	 * I don't understand single | &
-	 * 
-	 * 
-	 * 	
-	 */
+	//Statement about class
+		
 		
 		
 	}
