2bbed885ad5efeff152387b12d837dfa2e6f8166
Erin Bergman <erin.e.bergman@gmail.com>
2016-07-21 10:26:28 -0400

    07/21/16 10:26a

---
+++ src/com/bergman/Whatever.java
@@ -0,0 +1,19 @@
+package com.bergman;
+
+public class Whatever {
+
+	public static void main(String[] args) {
+// &&
+		
+	/*
+	 * *
+	 * I don't understand single | &
+	 * 
+	 * 
+	 * 	
+	 */
+		
+		
+	}
+
+}
